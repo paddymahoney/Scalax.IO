1fb60723f93d06112c5e2be270775922fb14ee0f
Andrew Foggin <andy@foggin.net>
2008-01-28 14:11:55 +0900

    Added standard copyright message to every file in scalax/rules

--- src/scalax/rules/IncrementalParser.scala
+++ src/scalax/rules/IncrementalParser.scala
@@ -1,3 +1,15 @@
+// -----------------------------------------------------------------------------
+//
+//  Scalax - The Scala Community Library
+//  Copyright (c) 2005-8 The Scalax Project. All rights reserved.
+//
+//  The primary distribution site is http://scalax.scalaforge.org/
+//
+//  This software is released under the terms of the Revised BSD License.
+//  There is NO WARRANTY.  See the file LICENSE for the full text.
+//
+// -----------------------------------------------------------------------------
+
 package scalax.rules
 
 trait IncrementalScanner extends Scanner with MemoisableRules {
--- src/scalax/rules/Rule.scala
+++
@@ -1,2 +0,0 @@
-package scalax.rules
-
