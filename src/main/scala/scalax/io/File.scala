5470c1885e4574d88ddfe0f59374d0255dc11591
Erik Engbrecht <erik.engbrecht@gmail.com>
2009-06-28 08:39:07 -0400

    refactored File into Location, File, Directory, and Path

--- src/main/scala/scalax/io/File.scala
+++ src/main/scala/scalax/io/File.scala
@@ -5,6 +5,16 @@ import collection.Traversable
 import util.matching.Regex
 import _root_.scalax.resource.ManagedResource
 
+
+/** stuff that should be in an object somewhere.... */
+trait Stuff {
+  def createTempFile(prefix: String, suffix: String): File
+  def userHome: Directory
+  def currentWorkingDirectory: Directory
+  def roots: Traversable[Location]
+}
+
+
 /**
  * TODO - Update this to work
  */
@@ -12,63 +22,146 @@ class GlobMapper(pattern : String) {
    def matches(path : String) : Boolean = false
 }
 
-//TODO - Complete this interface!!!
-trait File { self =>
-    def isDirectory : Boolean
-    def path : String
-    def cannonicalPath : String
-    def tree : Traversable[File]
-    def /(path : String) : File
-    def inputStream : InputStream
-    def outputStream : OutputStream
-    def delete() : Boolean
-    def exists : Boolean
-    def renameTo(name : File) : Boolean
+/**
+ * A point on a file system, such as a file, directory, or abstract pathname
+ */
+trait Location {
+  type Location_T <: Location
+  def canRead: Boolean
+  def canWrite: Boolean
+  def exists: Boolean
+  def name: String
+  def isAbsolute: Boolean
+  def isHidden: Boolean
+  /**
+   * the number of seconds since the Epoch when this <code>Location</code was modified
+   * @todo what should be returned if the file does not exist?
+   */
+  def lastModified: Long
 
-    
-    def copyTo(dest : File) : Unit = {
-       for {
-          in <- ManagedResource(inputStream)
-          out <- ManagedResource(outputStream)
-       } in >>> out
-    }
-    def moveTo(dest : File) : Unit = {
-      dest.delete()
-      if(!renameTo(dest)) {
-        copyTo(dest)
-        if(!delete()) throw error("Delete after copy failed: "+self) //TODO - Use IOException!
-      }
-    }
-
-
-    def pathFrom(file : File) : String = if(cannonicalPath.startsWith(file.cannonicalPath)) path.drop(file.cannonicalPath.length)  else cannonicalPath
-
-    def /(glob : GlobMapper) = new Traversable[File] {
-       def foreach[U](f : File => U) = self.tree.filter(x => glob.matches(x.pathFrom(self))).foreach(f)
-    }
-    def /(regex : Regex) = new Traversable[File] {
-       def foreach[U](f : File => U) = self.tree.filter( x => regex.pattern.matcher(x.pathFrom(self)).matches).foreach(f)
-    }
-
-
-
-    def reader(implicit charset : Charset = Charset.forName("UTF-8")) = inputStream.reader(charset)
-    def writer(implicit charset : Charset = Charset.forName("UTF-8")) = outputStream.writer(charset)
- 
-    def lines(implicit charset : Charset = Charset.forName("UTF-8")) : Traversable[String] = ManagedStreams.lines(reader(charset).buffered)(LineEndingStyle.ALL) //TODO - Figure out implicit/defaults issue!!!
-    def chars(implicit charset : Charset = Charset.forName("UTF-8")) : Traversable[Char] = ManagedStreams.chars(reader(charset).buffered)
-    def bytes : Traversable[Byte] = ManagedStreams.bytes(inputStream.buffered)
-    def slurp : Array[Byte] = for(in <- ManagedResource(inputStream)) yield in.slurp
-
-    def writeLines(lines : Iterable[String])(implicit charset : Charset = Charset.forName("UTF-8")) : Unit = for(out <- ManagedResource(writer)) {
-          out.writeLines(lines)
-    }
-    def write(input : String)(implicit charset : Charset = Charset.forName("UTF-8")) : Unit = for(out <- ManagedResource(writer)) {
-          out.writeChars(input)
-    }
-    def write(input : Array[Byte]) : Unit = for(out <- ManagedResource(outputStream)) {
-          out.write(input)()
-    }
+  /**
+   * true if this <code>Location</code> represents a directory
+   * Note the small "d" in "directory," this location may be a <code>Path</code>
+   * which can represent either a file or directory on the filesystem, but is neither
+   * a <code>File</code> or <code>Directory</code> object.
+   */
+  def isDirectory: Boolean
+  /** true if this <code>Location</code> represents a file */
+  def isFile: Boolean
+  /** the directory containing this <code>Location</code> */
+  def parent: Option[Directory]
+  def path: String
+  def cannonicalPath: String
+  def delete(): Boolean
+  def renameTo(name: Location_T): Boolean
+  /**
+   * @return path to this <code>Location</code> relative to the specified <code>Directory</code>
+   */
+  def pathFrom(dir: Directory): String = {
+    if (cannonicalPath.startsWith(dir.cannonicalPath)) {
+      path.drop(dir.cannonicalPath.length)
+    } else cannonicalPath
+  }
 }
+
+trait Directory extends Location { self =>
+  type Location_T <: Directory
+  /** the full contents of this directory tree */
+  def tree: Traversable[Location]
+  /** the subdirectories immediately under this directory */
+  def children: Traversable[Directory]
+  /** the <code>File</code>s within this <code>Directory</code>*/
+  def files: Traversable[File]
+  def contents: Traversable[Location]
+  /**
+   * atomically create new empty Location with this pathname if one does not exist
+   * @return true if a file was created, false if not
+   */
+  def create: Boolean
+  final def isDirectory = true
+  final def isFile = false
+
+  def /(path: String): Path
+  def /(glob: GlobMapper) = new Traversable[Location] {
+     def foreach[U](f: Location => U) = self.tree.filter(x => glob.matches(x.pathFrom(self))).foreach(f)
+  }
+  def /(regex: Regex) = new Traversable[Location] {
+     def foreach[U](f: Location => U) = self.tree.filter( x => regex.pattern.matcher(x.pathFrom(self)).matches).foreach(f)
+  }
+}
+
+//TODO - Complete this interface!!!
+trait File extends Location { self =>
+  type Location_T <: File
+  final def isDirectory = false
+  final def isFile = true
+  /**
+   * the length of the this <code>File</code> in bytes
+   * @todo what should this return if the file does not exist?  java.io.File will return 0L,
+   *       the same as for an empty file.
+   */
+  def length: Long
+  /**
+   * atomically create new empty Location with this pathname if one does not exist
+   * @return true if a file was created, false if not
+   */
+  def create(): Boolean
+  def inputStream: InputStream
+  def outputStream: OutputStream
+//  def copyTo(dest: File): Unit
+//  def moveTo(dest: Location): Unit
+//TODO: make charset a constant somewhere instead of using forName repeated
+  def reader(implicit charset : Charset = Charset.forName("UTF-8")) = inputStream.reader(charset)
+  def writer(implicit charset : Charset = Charset.forName("UTF-8")) = outputStream.writer(charset)
+
+  def lines(implicit charset : Charset = Charset.forName("UTF-8")) : Traversable[String] = ManagedStreams.lines(reader(charset).buffered)(LineEndingStyle.ALL) //TODO - Figure out implicit/defaults issue!!!
+  def chars(implicit charset : Charset = Charset.forName("UTF-8")) : Traversable[Char] = ManagedStreams.chars(reader(charset).buffered)
+  def bytes : Traversable[Byte] = ManagedStreams.bytes(inputStream.buffered)
+  def slurp : Array[Byte] = for(in <- ManagedResource(inputStream)) yield in.slurp
+
+  def writeLines(lines : Iterable[String])(implicit charset : Charset = Charset.forName("UTF-8")) : Unit = for(out <- ManagedResource(writer)) {
+        out.writeLines(lines)
+  }
+  def write(input : String)(implicit charset : Charset = Charset.forName("UTF-8")) : Unit = for(out <- ManagedResource(writer)) {
+        out.writeChars(input)
+  }
+  def write(input : Array[Byte]) : Unit = for(out <- ManagedResource(outputStream)) {
+        out.write(input)()
+  }
+}
+
+
+trait Path extends Location {
+  type Location_T <: Path
+  /**
+   * Convert this <code>Path</code> into a <code>Directory</code<
+   * @return a <code>Directory</code> object representing this path
+   * @throws IllegalStateException if this path is already a file
+   */
+  def asDirectory: Directory
+  /**
+   * Convert this <code>Path</code> into a <code>File</code>
+   * @return a <code>File</code> object representing this path
+   * @throws IllegalStateException if this path already exists as a directory
+   */
+  def asFile: File
+  /**
+   * @returns <code>None</code> if the path doesn't exist, or an appropriate typed object if it does
+   */
+  def asFileOrDirectory: Option[Either[File, Directory]]
+  /**
+   * Creates a new file if this <code>Path</code> does not exist
+   * @return <code>Some(file)</code> if a file was created, otherwise <code>None</code>
+   */
+  def createNewFile(): Option[File]
+  /**
+   * Creates a new directory if this <code>Path</code> does not exist
+   * @return <code>Some(directory)</code> if a directory was created, otherwise <code>None</code>
+   */
+  def createNewDirectory(): Option[Directory]
+}
+
+
+
 
 
--- src/main/scala/scalax/io/JavaFileHelper.scala
+++ src/main/scala/scalax/io/JavaFileHelper.scala
@@ -9,27 +9,85 @@ import _root_.scalax.resource.ManagedResource
 
 import java.{io => jio}
 
-class JavaFileWrapper(file : jio.File) extends File {
-   def isDirectory = file.isDirectory()
-   def path = file.getPath
-   def cannonicalPath = file.getCanonicalPath
-   def /(path : String) = new JavaFileWrapper(new jio.File(file, path))
-   def tree : Traversable[File] = new Traversable[File] {
-        def foreach[U](f : File => U) = (new JFileTree(file)).foreach(jf => f(new JavaFileWrapper(jf)))
-   }
-   def inputStream = new JavaInputStreamWrapper(new jio.FileInputStream(file))
-   def outputStream = new JavaOutputStreamWrapper(new jio.FileOutputStream(file))
+trait JavaLocation extends Location {
+  type Location_T <: JavaLocation
+  protected val file: jio.File
+  final def canRead = file.canRead
+  final def canWrite = file.canWrite
+  final def exists = file.exists
+  final def name = file.getName()
+  final def isAbsolute = file.isAbsolute
+  final def isHidden = file.isHidden
+  final def lastModified = file.lastModified
+  final def parent: Option[JavaDirectory] = {
+    file.getParentFile match {
+      case null => None
+      case dir => Some(new JavaDirectory(dir))
+    }
+  }
+  final def path = file.getPath
+  final def cannonicalPath = file.getCanonicalPath
+  final def delete() = file.delete()
+  def renameTo(name: Location_T) = file.renameTo(name.file)
+}
 
-   def delete() : Boolean = file.delete()
-   def renameTo(name : File) : Boolean = file.renameTo(new jio.File(name.cannonicalPath))
-   def exists = file.exists()
+trait JavaDirectoryMixin extends JavaLocation {
+  def /(path : String) = new JavaPath(new jio.File(file, path))
+  def tree: Traversable[JavaLocation] = new Traversable[JavaLocation] {
+    def foreach[U](f: JavaLocation => U) = (new JFileTree(file)).foreach(jf => f(new JavaPath(jf)))
+  }
+  def children: Traversable[JavaDirectory] = file.listFiles.filter(_.isDirectory).map(new JavaDirectory(_))
+  def files: Traversable[JavaFile] = file.listFiles.filter(_.isFile).map(new JavaFile(_))
+  def contents: Traversable[JavaLocation] = file.listFiles.map(new JavaPath(_))
+}
 
+trait JavaFileMixin extends JavaLocation {
+  def length = file.length
+  def inputStream = new JavaInputStreamWrapper(new jio.FileInputStream(file))
+  def outputStream = new JavaOutputStreamWrapper(new jio.FileOutputStream(file))
+}
+
+final class JavaDirectory(protected val file: jio.File) extends Directory with JavaDirectoryMixin {
+  type Location_T = JavaDirectory
+  def create = file.mkdirs
+}
+
+final class JavaFile(protected val file: jio.File) extends File with JavaFileMixin {
+  type Location_T = JavaFile
+  def create() = file.createNewFile()
+}
+
+final class JavaPath(protected val file: jio.File) extends Path with JavaFileMixin with JavaDirectoryMixin {
+  type Location_T = JavaPath
+  def asDirectory = {
+    if (isDirectory || !exists) new JavaDirectory(file)
+    else throw new UnsupportedOperationException("this Path is already a file, cannot convert to a directory")
+  }
+  def asFile = {
+    if (isFile || !exists) new JavaFile(file)
+    else throw new UnsupportedOperationException("this Path is already a directory, cannot convert to a file")
+  }
+  def asFileOrDirectory: Option[Either[JavaFile, JavaDirectory]] = {
+    if (!exists) None
+    else if (isDirectory) Some(Right(new JavaDirectory(file)))
+    else Some(Left(new JavaFile(file)))
+  }
+  def createNewFile() = if (file.createNewFile()) Some(new JavaFile(file)) else None
+  def createNewDirectory() = if (file.mkdirs()) Some(new JavaDirectory(file)) else None
+  def isDirectory = file.isDirectory
+  def isFile = file.isFile
 }
 
 object FileConstants {
-  lazy val tmpDir = new JavaFileWrapper(new jio.File(System.getProperty("java.io.tmpdir")))
+  // EE: I made tmpDir a def rather than a lazy val because:
+  //     (1) it returns a mutable object, and
+  //     (2) someone could change the property "java.io.tmpdir"
+  //     so you really want a new object every time
+  def tmpDir = new JavaDirectory(new jio.File(System.getProperty("java.io.tmpdir")))
 }
 
+// Note: calling list() on a file (as in not a directory) returns null
+//       calling list() on an empty directory returns an empty array
 
 import scala.collection.mutable.Queue
 /** Represents the preorder traversal of the directory tree rooted at the given file. */
